1da0a275343169690b43d2d722646901b1917716
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-11 14:05:43 -0500

    Organized the output of the game; making similar variables

--- cogs/rps.py
+++ cogs/rps.py
@@ -15,10 +15,7 @@ class rps(commands.Cog):
 		userChoice = userChoice.lower()
 		if userChoice == "rock" or userChoice == "paper" or userChoice == "scissors":
 			if await self.bot.get_cog("Economy").subtractBet(ctx, amntBet) != 0:
-				botChoice = ['rock', 'paper', 'scissors']
-				botChoice = random.choice(botChoice)
-
-				winner = 10
+				botChoice = random.choice(['rock', 'paper', 'scissors'])
 
 				if userChoice == botChoice:
 					winner = 0
@@ -41,33 +38,44 @@ class rps(commands.Cog):
 				elif userChoice == "scissors" and botChoice == "rock":
 					winner = -1
 
-
+				multiplier = self.bot.get_cog("Economy").getMultiplier(ctx)
+				embed = discord.Embed(color=0xff2020)
 				if winner == 1:
-					await self.bot.get_cog("Economy").addWinnings(ctx.author.id, amntBet*2)
-					balance = self.bot.get_cog("Economy").getBalance(ctx.author.id)
-					multiplier = await self.bot.get_cog("Economy").getMultiplier(ctx)
-					embed = discord.Embed(color=0x23f518, type="rich")
-					embed.add_field(name=f"Pit Boss' Casino | RPS", value=f"**{ctx.author.name}** picked **{userChoice}** \n**Bot** picked **{botChoice}**",inline=False)
-					embed.add_field(name="-----------------------------------------------------------------", 
-						value = f"Congratulations, you just won {amntBet*2} (+{(amntBet*2) * multiplier}){coin}!\n**Credits**: {balance}{coin}", inline=False)
-					await ctx.send(embed=embed)
+					moneyToAdd = amntBet * 2 
+					profitInt = moneyToAdd - amntBet
+					result = "YOU WON"
+					profit = f"**{profitInt}** (**+{int(profitInt * (multiplier - 1))}**)"
+					
+					embed.color = discord.Color(0x23f518)
 
 				elif winner == -1:
-					balance = self.bot.get_cog("Economy").getBalance(ctx.author.id)
-					embed = discord.Embed(color=0xff2020, type="rich")
-					embed.add_field(name=f"Pit Boss' Casino | RPS", value=f"**{ctx.author.name}** picked **{userChoice}** \n**Bot** picked **{botChoice}**",inline=False)
-					embed.add_field(name="-----------------------------------------------------------------", 
-						value = f"Sorry, you didn't win anything.\n**Credits**: {balance}{coin}", inline=False)
-					await ctx.send(embed=embed)
+					moneyToAdd = 0 # nothing to add since loss
+					profitInt = -amntBet # profit = amntWon - amntBet; amntWon = 0 in this case
+					result = "YOU LOST"
+					profit = f"**{profitInt}**"
 
+				
 				elif winner == 0:
-					await self.bot.get_cog("Economy").addWinnings(ctx.author.id, amntBet)
-					balance = self.bot.get_cog("Economy").getBalance(ctx.author.id)
-					embed = discord.Embed(color=1768431, type="rich")
-					embed.add_field(name=f"Pit Boss' Casino | RPS", value=f"**{ctx.author.name}** picked **{userChoice}** \n**Bot** picked **{botChoice}**",inline=False)
-					embed.add_field(name="-----------------------------------------------------------------", 
-						value = f"It's a tie. At least you get your {amntBet}{coin} back!\n**Credits**: {balance}{coin}", inline=False)
-					await ctx.send(embed=embed)
+					moneyToAdd = amntBet # add back their bet they placed since it was pushed (tied)
+					profitInt = 0 # they get refunded their money (so they don't make or lose money)
+					result = "PUSHED"
+					profit = f"**{profitInt}**"
+
+				embed.add_field(name=f"Pit Boss' Casino | RPS", value=f"**{ctx.author.name}** picked **{userChoice}** \n**Pit Boss** picked **{botChoice}**",inline=False)
+				giveZeroIfNeg = max(0, profitInt) # will give 0 if profit is negative. 
+																						# we don't want it subtracting anything, only adding
+				await self.bot.get_cog("Economy").addWinnings(ctx.author.id, moneyToAdd + (giveZeroIfNeg * (multiplier - 1)))
+				balance = self.bot.get_cog("Economy").getBalance(ctx.author.id)
+				embed.add_field(name="Profit", value=f"{profit}{coin}", inline=True)
+				embed.add_field(name="Credits", value=f"**{balance}**{coin}", inline=True)
+
+				await self.bot.get_cog("Totals").addTotals(ctx, amntBet, moneyToAdd, 5)
+				xp = random.randint(50, 500)
+				embed.set_footer(text=f"Earned {xp} XP!")
+				await self.bot.get_cog("XP").addXP(ctx, xp)
+				await ctx.send(content=f"{ctx.message.author.mention}", embed=embed)
+
+
 
 		else:
 			await ctx.send("Incorrect choice.")
