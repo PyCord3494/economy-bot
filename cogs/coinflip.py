bf04821bb541c5c5bf06121fe7fc945bb3446c3b
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-11 17:01:19 -0500

    Added thumbnails for win/lose

--- cogs/coinflip.py
+++ cogs/coinflip.py
@@ -10,7 +10,7 @@ class Coinflip(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(aliases=['flipcoin', 'flip', 'coin'], pass_context=True)
+	@commands.command(aliases=['flipcoin', 'flip', 'coin', 'cf'], pass_context=True)
 	@commands.cooldown(1, 3, commands.BucketType.user)
 	async def coinflip(self, ctx, sideBet: str, amntBet: int):
 		if await self.bot.get_cog("Economy").subtractBet(ctx, amntBet) != 0:
@@ -20,19 +20,25 @@ class Coinflip(commands.Cog):
 				side = random.choice(['Heads', 'Tails'])
 				
 				multiplier = self.bot.get_cog("Economy").getMultiplier(ctx)
+
 				embed = discord.Embed(color=0x23f518)
+				#embed.set_thumbnail(url="https://png2.kisspng.com/sh/a69196ddd7a980602722dfec7d75a1db/L0KzQYm3VcIyN5Dvh5H0aYP2gLBuTgNqdKdqip9sb3nxPbb0jBpqNZZyhAZyY3BxPbj2jPQuPZM1S6RvMUS1cYHrWMIvPmc9UKk7NUS0RYO9WcM2O2I7Sak7Mz7zfri=/kisspng-silver-coin-emoji-emoticon-gold-5b032f142a0d82.6688725415269353161723.png")
 				if sideBet.lower() == side.lower():
 					moneyToAdd = int(amntBet * 2)
 					profitInt = moneyToAdd - amntBet
 					profit = f"**{profitInt}** (**+{int(profitInt * (multiplier - 1))}**)"
+
+					file = discord.File("coinwon.png", filename="image.png")
 
 				else:
 					moneyToAdd = 0
 					profitInt = moneyToAdd - amntBet
 					profit = f"**{profitInt}**"
 
+					file = discord.File("coinlost.png", filename="image.png")
 					embed.color = discord.Color(0xff2020)
-
+					
+				embed.set_thumbnail(url="attachment://image.png")
 				embed.add_field(name=f"Pit Boss' Casino | Coinflip", value=f"The coin landed on {side}\n_ _",inline=False)
 				giveZeroIfNeg = max(0, profitInt) # will give 0 if profitInt is negative. 
 																				# we don't want it subtracting anything, only adding
@@ -45,7 +51,7 @@ class Coinflip(commands.Cog):
 				xp = random.randint(45, 475)
 				embed.set_footer(text=f"Earned {xp} XP!")
 				await self.bot.get_cog("XP").addXP(ctx, xp)
-				await ctx.send(embed=embed)
+				await ctx.send(file=file, embed=embed)
 
 	@coinflip.error
 	async def coinflip_handler(self, ctx, error):
