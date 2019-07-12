416b181f86f681c4046bf9888ce46e5f64003fee
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-11 22:29:57 -0500

    Reformatted level cmd & added more text to display for leveling up

--- cogs/xp.py
+++ cogs/xp.py
@@ -30,7 +30,6 @@ class XP(commands.Cog):
 		level = getRow[3]
 		xp = getRow[1]
 		requiredXP = self.XPtoLevelUp[level]
-		minimumBet = 200
 		levelReward = getRow[0]
 		progress = round((xp / requiredXP) * 100)
 		totalXP = getRow[1]
@@ -39,18 +38,17 @@ class XP(commands.Cog):
 
 		embed = discord.Embed(color=1768431, title="Pit Boss' Casino | Level")
 		embed.add_field(name = "Level", value = f"You are level **{level}**", inline=True)
-		embed.add_field(name = "XP / Next Level", value = f"**{xp}** / **{requiredXP}**", inline=True)
-		embed.add_field(name = "Minimum Bet", value = f"**{minimumBet}** credits", inline=True)
 		embed.add_field(name = "Level Reward", value = f"**{levelReward}**", inline=True)
-		embed.add_field(name = "Progress", value = f"**{progress}%**", inline=True)
 		embed.add_field(name = "Total XP", value = f"**{totalXP}**", inline=True)
+		embed.add_field(name = "XP / Next Level", value = f"**{xp}** / **{requiredXP}**", inline=True)
+		embed.add_field(name = "XP Until Level Up", value = f"**{requiredXP - xp}**", inline=True)
+		embed.add_field(name = "Progress", value = f"**{progress}%**", inline=True)
 		await ctx.send(embed=embed)
 
 
 	async def addXP(self, ctx, xp):
 		db = pymysql.connect(host="twister.hostingspark.net",port=3306, user="hostings_autop",passwd="pwqA!Pp9!1",db="hostings_botdatabase",autocommit=True)
 		cursor = db.cursor()
-
 
 		sql = f"""Update Economy
 				  SET XP = XP + {xp}
@@ -85,12 +83,29 @@ class XP(commands.Cog):
 				  WHERE DiscordID = '{discordId}';"""
 			cursor.execute(sql)
 			db.commit()
-			await ctx.send(ctx.author.mention + ", you Leveled Up!")
-			role = discord.utils.get(ctx.guild.roles, name = "Level 1")
-			await ctx.author.add_roles(role)
-			await asyncio.sleep(0.5)
-			role = discord.utils.get(ctx.guild.roles, name = "Level 0")
-			await ctx.author.remove_roles(role)
+			embed = discord.Embed(color=1768431, title="Level Up!")
+			file = discord.File("./images/levelup.png", filename="image.png")
+			embed.set_thumbnail(url="attachment://image.png")
+			embed.add_field(name = f"{ctx.author.name}, you Leveled Up!", value = "_ _", inline=False)
+			embed.add_field(name = f"Level Reward", value = f"**{self.levelReward[level]} ---> {self.levelReward[level + 1]}**:confetti_ball::confetti_ball:", inline=False)
+			await ctx.send(file=file, embed=embed)
+			#print(level + 1)
+			#role = discord.utils.get(ctx.guild.roles, name = f"Level {level + 1}")
+			#await ctx.author.add_roles(role)
+			#await asyncio.sleep(0.5)
+			#role = discord.utils.get(ctx.guild.roles, name = f"Level {level}")
+			#await ctx.author.remove_roles(role)
+
+# HERE FOR TESTING PURPOSES FOR IMAGES AND THUMBNAILS
+	# @commands.command(pass_context=True)
+	# async def test(self, ctx):
+	# 	embed = discord.Embed(color=1768431, title="Level Up!")
+	# 	file = discord.File("./images/timeout.png", filename="image.png")
+	# 	embed.set_thumbnail(url="attachment://image.png")
+	# 	embed.add_field(name = f"{ctx.author.name}, you Leveled Up!", value = "_ _", inline=False)
+	# 	embed.add_field(name = f"Level Reward", value = f"**550 ---> 1500** :confetti_ball::confetti_ball: ", inline=False)
+	# 	await ctx.send(file=file, embed=embed)
+
 
 
 		
