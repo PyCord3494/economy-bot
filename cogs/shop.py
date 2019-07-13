901e07c17b169d4a3fc2fc02d96925aa2ea5fffc
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-13 14:04:18 -0500

    Added rest of the code for shop items

--- cogs/shop.py
+++ cogs/shop.py
@@ -7,7 +7,7 @@ import asyncio
 class Shop(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.items = [1000, 5000, 50000, 100000, 150000, 35000, 250000]
+		self.items = [1000, 5000, 50000, 100000, 150000, 250000, 35000]
 
 
 	@commands.group(invoke_without_command=True)
@@ -20,8 +20,8 @@ class Shop(commands.Cog):
 									+ "2             1 key                5,000\n"
 									+ "3        +1,000 to daily          50,000\n"
 									+ "4       +500 to lvl reward       100,000\n"
-									+ "5      +5,000 to vote reward     150,000\n"
-									+ "6   +1.5x Profit on Future Games 300,000\n"
+									+ "5      ---------------------     150,000\n"
+									+ "6   +1.5x Profit on Future Games 250,000\n"
 									+ "7     +2,500 to donator reward    35,000\n"
 									+ "----------------------------------------\n"
 									+ "Use +shop buy <id> <amount>\n```")
@@ -33,8 +33,8 @@ class Shop(commands.Cog):
 									+ "2            +1 key                5,000\n"
 									+ "3        +1,000 to daily          50,000\n"
 									+ "4       +500 to lvl reward       100,000\n"
-									+ "5      +5,000 to vote reward     150,000\n"
-									+ "6   +1.5x Profit on Future Games 300,000\n"
+									+ "5      ---------------------     150,000\n"
+									+ "6   +50% Profit on Future Games 250,000\n"
 									+ "----------------------------------------\n"
 									+ "Use +shop buy <id> <amount>\n```")
 
@@ -44,46 +44,58 @@ class Shop(commands.Cog):
 		cost = self.items[ID] * amnt
 		balance = self.bot.get_cog("Economy").getBalance(ctx.author.id) > price
 		if balance > price:
-			await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
-			db = pymysql.connect(host="twister.hostingspark.net",port=3306, user="hostings_autop",passwd="pwqA!Pp9!1",db="hostings_botdatabase",autocommit=True)
-			cursor = db.cursor()
-			
-			if ID == 1:
-				pass
+			if ID < 8 and ID > 0:
+				await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
+				db = pymysql.connect(host="twister.hostingspark.net",port=3306, user="hostings_autop",passwd="pwqA!Pp9!1",db="hostings_botdatabase",autocommit=True)
+				cursor = db.cursor()
+				
+				if ID == 1:
+					sql = f"""UPDATE Inventory
+						  SET Crates = Crates + {amnt}
+						  WHERE DiscordID = '{discordId}';"""
+					cursor.execute(sql)
+					db.commit()
 
-			elif ID == 2:
-				pass
+				elif ID == 2:
+					sql = f"""UPDATE Inventory
+						  SET Keys = Keys + {amnt}
+						  WHERE DiscordID = '{discordId}';"""
+					cursor.execute(sql)
+					db.commit()
 
-			elif ID == 3:
-				sql = f"""UPDATE Economy
-					  SET DailyReward = DailyReward + 1000
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()	
+				elif ID == 3:
+					sql = f"""UPDATE Economy
+						  SET DailyReward = DailyReward + 1000
+						  WHERE DiscordID = '{discordId}';"""
+					cursor.execute(sql)
+					db.commit()	
 
-			elif ID == 4:
-				sql = f"""UPDATE Economy
-					  SET Multiplier = Multiplier + 2
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()	
+				elif ID == 4:
+					sql = f"""UPDATE Economy
+						  SET LevelReward = LevelReward + 500
+						  WHERE DiscordID = '{discordId}';"""
+					cursor.execute(sql)
+					db.commit()	
 
-			elif ID == 5:
-				pass
+				elif ID == 5:
+					pass
 
-			elif ID == 6:
-				sql = f"""UPDATE Economy
-					  SET Multiplier = Multiplier + 2
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()	
+				elif ID == 6:
+					sql = f"""UPDATE Economy
+						  SET Multiplier = Multiplier + 0.5
+						  WHERE DiscordID = '{discordId}';"""
+					cursor.execute(sql)
+					db.commit()	
 
-			elif ID == 7:
-				sql = f"""UPDATE Economy
-					  SET Multiplier = Multiplier + 2
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()				
+				elif ID == 7 and await self.bot.get_cog("Economy").isDonator(discordId) == 1:
+					sql = f"""UPDATE Economy
+						  SET Multiplier = DonatorReward + 2500
+						  WHERE DiscordID = '{discordId}';"""
+					cursor.execute(sql)
+					db.commit()	
+
+				else:
+					await ctx.send("Invalid choice.")	
 
 			db.close()
 		else:
