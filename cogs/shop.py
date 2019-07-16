70fba68131aaeb8387b94894476884f265dcaed7
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-15 20:49:38 -0500

    Made flow of code smoother; added the accCheck() code

--- cogs/shop.py
+++ cogs/shop.py
@@ -12,94 +12,101 @@ class Shop(commands.Cog):
 
 	@commands.group(invoke_without_command=True, pass_context=True)
 	async def shop(self, ctx):
-		if ctx.invoked_subcommand is None:
-			if self.bot.get_cog("Economy").isDonator(ctx.author.id) == 1:
-				await ctx.send("```ML\nID            ITEMS                COST\n"
-									+ "----------------------------------------\n"
-									+ "1            1 crate               1,000\n"
-									+ "2             1 key                5,000\n"
-									+ "3        +1,000 to daily          50,000\n"
-									+ "4       +500 to lvl reward       100,000\n"
-									+ "5      ---------------------     150,000\n"
-									+ "6   +1.5x Profit on Future Games 250,000\n"
-									+ "7     +2,500 to donator reward    35,000\n"
-									+ "----------------------------------------\n"
-									+ "Use +shop buy <id> <amount>\n```")
+		if await self.bot.get_cog("Economy").accCheck(ctx) == True:
+			if ctx.invoked_subcommand is None:
+				if self.bot.get_cog("Economy").isDonator(ctx.author.id) == 1:
+					await ctx.send("```ML\nID            ITEMS                COST\n"
+										+ "----------------------------------------\n"
+										+ "1            1 crate               1,000\n"
+										+ "2             1 key                5,000\n"
+										+ "3        +1,000 to daily          50,000\n"
+										+ "4       +500 to lvl reward       100,000\n"
+										+ "5      ---------------------     150,000\n"
+										+ "6   +1.5x Profit on Future Games 250,000\n"
+										+ "7     +2,500 to donator reward    35,000\n"
+										+ "----------------------------------------\n"
+										+ "Use +shop buy <id> <amount>\n```")
 
-			else:
-				await ctx.send("```ML\nID            ITEMS                COST\n"
-									+ "----------------------------------------\n"
-									+ "1           +1 crate               1,000\n"
-									+ "2            +1 key                5,000\n"
-									+ "3        +1,000 to daily          50,000\n"
-									+ "4       +500 to lvl reward       100,000\n"
-									+ "5      ---------------------     150,000\n"
-									+ "6   +50% Profit on Future Games 250,000\n"
-									+ "----------------------------------------\n"
-									+ "Use +shop buy <id> <amount>\n```")
+				else:
+					await ctx.send("```ML\nID            ITEMS                COST\n"
+										+ "----------------------------------------\n"
+										+ "1           +1 crate               1,000\n"
+										+ "2            +1 key                5,000\n"
+										+ "3        +1,000 to daily          50,000\n"
+										+ "4       +500 to lvl reward       100,000\n"
+										+ "5      ---------------------     150,000\n"
+										+ "6   +50% Profit on Future Games 250,000\n"
+										+ "----------------------------------------\n"
+										+ "Use +shop buy <id> <amount>\n```")
+		else:
+			await ctx.send("Hello! Please type $start to create your wallet. :smiley:")
+			return 0
 
 	@shop.command()
 	async def buy(ctx, ID: int, amnt: int):
-		discordId = ctx.author.id
-		cost = self.items[ID] * amnt
-		balance = self.bot.get_cog("Economy").getBalance(ctx.author.id)
-		if balance > price:
-			if ID < 8 and ID > 0:
-				await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
-				db = pymysql.connect(host="twister.hostingspark.net",port=3306, user="hostings_autop",passwd="pwqA!Pp9!1",db="hostings_botdatabase",autocommit=True)
-				cursor = db.cursor()
-				
-				if ID == 1:
-					sql = f"""UPDATE Inventory
-						  SET Crates = Crates + {amnt}
-						  WHERE DiscordID = '{discordId}';"""
-					cursor.execute(sql)
-					db.commit()
+		if await self.bot.get_cog("Economy").accCheck(ctx) == True:
+			discordId = ctx.author.id
+			cost = self.items[ID] * amnt
+			balance = await self.bot.get_cog("Economy").getBalance(ctx)
+			if balance >= price:
+				if ID <= 7 and ID > 0:
+					await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
+					db = pymysql.connect(host="twister.hostingspark.net",port=3306, user="hostings_autop",passwd="pwqA!Pp9!1",db="hostings_botdatabase",autocommit=True)
+					cursor = db.cursor()
+					
+					if ID == 1:
+						sql = f"""UPDATE Inventory
+							  SET Crates = Crates + {amnt}
+							  WHERE DiscordID = '{discordId}';"""
+						cursor.execute(sql)
+						db.commit()
 
-				elif ID == 2:
-					sql = f"""UPDATE Inventory
-						  SET Keys = Keys + {amnt}
-						  WHERE DiscordID = '{discordId}';"""
-					cursor.execute(sql)
-					db.commit()
+					elif ID == 2:
+						sql = f"""UPDATE Inventory
+							  SET Keys = Keys + {amnt}
+							  WHERE DiscordID = '{discordId}';"""
+						cursor.execute(sql)
+						db.commit()
 
-				elif ID == 3:
-					sql = f"""UPDATE Economy
-						  SET DailyReward = DailyReward + 1000
-						  WHERE DiscordID = '{discordId}';"""
-					cursor.execute(sql)
-					db.commit()	
+					elif ID == 3:
+						sql = f"""UPDATE Economy
+							  SET DailyReward = DailyReward + 1000
+							  WHERE DiscordID = '{discordId}';"""
+						cursor.execute(sql)
+						db.commit()	
 
-				elif ID == 4:
-					sql = f"""UPDATE Economy
-						  SET LevelReward = LevelReward + 500
-						  WHERE DiscordID = '{discordId}';"""
-					cursor.execute(sql)
-					db.commit()	
+					elif ID == 4:
+						sql = f"""UPDATE Economy
+							  SET LevelReward = LevelReward + 500
+							  WHERE DiscordID = '{discordId}';"""
+						cursor.execute(sql)
+						db.commit()	
 
-				elif ID == 5:
-					pass
+					elif ID == 5:
+						pass
 
-				elif ID == 6:
-					sql = f"""UPDATE Economy
-						  SET Multiplier = Multiplier + 0.5
-						  WHERE DiscordID = '{discordId}';"""
-					cursor.execute(sql)
-					db.commit()	
+					elif ID == 6:
+						sql = f"""UPDATE Economy
+							  SET Multiplier = Multiplier + 0.5
+							  WHERE DiscordID = '{discordId}';"""
+						cursor.execute(sql)
+						db.commit()	
 
-				elif ID == 7 and await self.bot.get_cog("Economy").isDonator(discordId) == 1:
-					sql = f"""UPDATE Economy
-						  SET Multiplier = DonatorReward + 2500
-						  WHERE DiscordID = '{discordId}';"""
-					cursor.execute(sql)
-					db.commit()	
+					elif ID == 7 and await self.bot.get_cog("Economy").isDonator(discordId) == 1:
+						sql = f"""UPDATE Economy
+							  SET Multiplier = DonatorReward + 2500
+							  WHERE DiscordID = '{discordId}';"""
+						cursor.execute(sql)
+						db.commit()	
 
+
+					db.close()
 				else:
-					await ctx.send("Invalid choice.")	
-
-			db.close()
+					await ctx.send("Invalid item ID.")
+			else:
+				await ctx.send(f"That will cost you {cost} credits, but you only have {balance} credits")
 		else:
-			ctx.send(f"That will cost you {cost} credits, but you only have {balance} credits")
+			await ctx.send("Hello! Please type $start to create your wallet. :smiley:")
 
 
 	@shop.command()
@@ -114,15 +121,18 @@ class Shop(commands.Cog):
 
 	@crate.command()
 	async def open(self, ctx, amnt=1):
-		crates, keys = await self.bot.get_cog("Economy").getInventory(ctx)
-		if crates >= amnt and keys >= amnt:
-			await self.bot.get_cog("Economy").subtractInv(ctx.author.id, amnt)
-			rCrates, rKeys = await self.bot.get_cog("Economy").getInventory(ctx.author.id)
-			balance = self.bot.get_cog("Economy").getBalance(ctx.author.id)
-			await ctx.send(f"You got **0** credits\nYou got **0** crates\nYou got **0** keys\n\nYou now have **{balance}** credits, **{rCrates}** crates, and **{rKeys}** keys")
-			await ctx.send()
-		elif crates != False:
-			await ctx.send(f"\n{ctx.author.mention}, you have {crates} crates and {keys} keys.\nType $crate to learn how to obtain more.")
+		if await self.bot.get_cog("Economy").accCheck(ctx) == True:
+			crates, keys = await self.bot.get_cog("Economy").getInventory(ctx)
+			if crates >= amnt and keys >= amnt and amnt > 0:
+				await self.bot.get_cog("Economy").subtractInv(ctx.author.id, amnt)
+				crates = crates - amnt
+				keys = keys - amnt
+				balance = await self.bot.get_cog("Economy").getBalance(ctx)
+				await ctx.send(f"You got **0** credits\nYou got **0** crates\nYou got **0** keys\n\nYou now have **{balance}** credits, **{rCrates}** crates, and **{rKeys}** keys")
+			elif crates != False:
+				await ctx.send(f"\n{ctx.author.mention}, you have {crates} crates and {keys} keys.\nType $crate to learn how to obtain more.")
+		else:
+			await ctx.send("Hello! Please type $start to create your wallet. :smiley:")
 		
 
 
