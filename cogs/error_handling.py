240d7ee090fe62a6fb611b69c3db5b0ab96362a1
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-16 20:49:06 -0500

    Trying to get error handling under control...

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -15,30 +15,25 @@ class ErrorHandling(commands.Cog):
 		if isinstance(error, type(None)):
 			await ctx.send("yes")
 		elif isinstance(error, commands.errors.NotOwner):
-			await ctx.send(f"{error}", delete_after=5)
+			await ctx.send(f"{error}")
 		elif isinstance(error, discord.errors.Forbidden):
-			await ctx.send("I don't have permission to perform the action", delete_after=5)
+			await ctx.send("I don't have permission to perform the action")
 		elif isinstance(error, commands.errors.CommandNotFound):
 			await ctx.send("Command not found.")
 		elif isinstance(error.__cause__, discord.errors.NotFound):
 			await ctx.send("Error cause not found.")
 		elif isinstance(error, commands.errors.NoPrivateMessage):
-			await ctx.send("That command can not be run in PMs!",
-								   delete_after=5)
-			return
+			await ctx.send("That command can not be run in PMs!")
 		elif isinstance(error, commands.errors.DisabledCommand):
-			await ctx.send("Sorry, but that command is currently disabled!",
-								   delete_after=5)
+			await ctx.send("Sorry, but that command is currently disabled!")
 		elif isinstance(error, commands.errors.CheckFailure):
-			await ctx.send("Check failed. You probably don't have "
-								   "permission to do this.", delete_after=5)
+			await ctx.send("Check failed. You probably don't have permission to do this.")
 		elif isinstance(error, commands.errors.CommandOnCooldown):
-			await ctx.send(f"{error}", delete_after=5)
+			await ctx.send(f"{error}")
 		elif isinstance(error, (commands.errors.BadArgument, commands.errors.MissingRequiredArgument)):
-			await ctx.send(f"Bad argument: {' '.join(error.args)}", delete_after=5)
-		#else:
-		#	await ctx.send(f"An error happened. This has been logged and reported. Error: {error}",
-		#						   delete_after=5)
+			await ctx.send(f"Bad argument: {' '.join(error.args)}")
+		else:
+		 	await ctx.send(f"An error happened. This has been logged and reported. Error: {error}")
 
 
 def setup(bot):
