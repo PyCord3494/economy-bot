eaed2c2b190291c185e7538878f687d3c5a2db9a
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-18 07:55:19 -0500

    Disabling error handling temporarily until can get more precise errors

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -1,5 +1,6 @@
 import asyncio
 import discord
+import sys
 from discord.ext import commands
 
 
@@ -7,33 +8,33 @@ class ErrorHandling(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-		"""
-		Catch command errors.
-		"""
-		if isinstance(error, type(None)):
-			await ctx.send("yes")
-		elif isinstance(error, commands.errors.NotOwner):
-			await ctx.send(f"{error}")
-		elif isinstance(error, discord.errors.Forbidden):
-			await ctx.send("I don't have permission to perform the action")
-		elif isinstance(error, commands.errors.CommandNotFound):
-			await ctx.send("Command not found.")
-		elif isinstance(error.__cause__, discord.errors.NotFound):
-			await ctx.send("Error cause not found.")
-		elif isinstance(error, commands.errors.NoPrivateMessage):
-			await ctx.send("That command can not be run in PMs!")
-		elif isinstance(error, commands.errors.DisabledCommand):
-			await ctx.send("Sorry, but that command is currently disabled!")
-		elif isinstance(error, commands.errors.CheckFailure):
-			await ctx.send("Check failed. You probably don't have permission to do this.")
-		elif isinstance(error, commands.errors.CommandOnCooldown):
-			await ctx.send(f"{error}")
-		elif isinstance(error, (commands.errors.BadArgument, commands.errors.MissingRequiredArgument)):
-			await ctx.send(f"Bad argument: {' '.join(error.args)}")
-		else:
-		 	await ctx.send(f"An error happened. This has been logged and reported. Error: {error}")
+	# @commands.Cog.listener()
+	# async def on_command_error(self, ctx, error):
+	# 	"""
+	# 	Catch command errors.
+	# 	"""
+	# 	if isinstance(error, type(None)):
+	# 		await ctx.send("yes")
+	# 	elif isinstance(error, commands.errors.NotOwner):
+	# 		await ctx.send(f"{error}")
+	# 	elif isinstance(error, discord.errors.Forbidden):
+	# 		await ctx.send("I don't have permission to perform the action")
+	# 	elif isinstance(error, commands.errors.CommandNotFound):
+	# 		await ctx.send("Command not found.")
+	# 	elif isinstance(error.__cause__, discord.errors.NotFound):
+	# 		await ctx.send("Error cause not found.")
+	# 	elif isinstance(error, commands.errors.NoPrivateMessage):
+	# 		await ctx.send("That command can not be run in PMs!")
+	# 	elif isinstance(error, commands.errors.DisabledCommand):
+	# 		await ctx.send("Sorry, but that command is currently disabled!")
+	# 	elif isinstance(error, commands.errors.CheckFailure):
+	# 		await ctx.send("Check failed. You probably don't have permission to do this.")
+	# 	elif isinstance(error, commands.errors.CommandOnCooldown):
+	# 		await ctx.send(f"{error}")
+	# 	elif isinstance(error, (commands.errors.BadArgument, commands.errors.MissingRequiredArgument)):
+	# 		await ctx.send(f"Bad argument: {' '.join(error.args)}")
+	# 	else:
+	# 	 	await ctx.send(f"An error happened. This has been logged and reported. Error: {error}")
 
 
 def setup(bot):
