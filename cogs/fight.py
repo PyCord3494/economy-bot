49344c5f4d53a10d494558375074f25d1fe2b997
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-22 13:29:56 -0500

    Added functions; more game logic

--- cogs/fight.py
+++ cogs/fight.py
@@ -27,61 +27,16 @@ class Fight(commands.Cog):
 		try:
 			p1Lvl = await self.bot.get_cog("XP").getLevel(ctx.author.id)
 			p2Lvl = await self.bot.get_cog("XP").getLevel(member.id)
-
-			dmg = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-			p1Dmg = dmg[p1Lvl]
-			p2Dmg = dmg[p2Lvl]
 		except Exception as e:
 			await ctx.send(f"User does not have an account. Error: {e}")
+			return
 
 
 		# listed in order: player health, armor, checkArmor, potion
 		try:
 			p1 = [self.p1Health, self.p1Armor, self.p1CheckArmor, self.p1Potions]
 			p2 = [self.p2Health, self.p2Armor, self.p2CheckArmor, self.p2Potions]
-
-			#firstTurn = self.firstTurn(p1Lvl, p2Lvl)
-			turnNum = 0
-			while True:
-				act = randint(1, 100) # generate what they'll do on their turn
-				
-				if turnNum % 2 == 0: # player 1's turn
-					self.embed.color = discord.Color(0x007efd)
-					player = p1
-					pText = "Player 1"
-					opponent = p2
-					oText = "Player 2"
-
-				else:  # player 2's turn
-					self.embed.color = discord.Color(0xfd0006)
-					player = p2
-					pText = "Player 2"
-					opponent = p1
-					oText = "Player 1"
-
-				if act <= 70: # 70% chance to attack; dmg amount based on generated #
-					opponent[0] -= act
-					self.embed.add_field(name="DAMAGE", value=f"{pText} damaged {oText} for {act} damage!\nHe has {opponent[0]} health left!")
-					await asyncio.sleep(2)
-				elif act > 70: # 30% chance to heal; heal amount based on generated #
-					player[3] -= 1
-					player[0] += act
-					self.embed.add_field(name="HEALED", value=f"{pText} healed for {act} health!\nHealth Remaining: {player[0]}\nPotions remaining: {player[0]}")
-					await asyncio.sleep(2)
-				
-				# sends embed and resets it for next turn
-				await ctx.send(embed=self.embed)
-				self.embed.clear_fields()
-
-				turnNum += 1 # changes whose turn it is
-
-				if self.checkGameOver(p1, p2):
-					#print("broke")
-					break # will break once player is dead
-				await asyncio.sleep(2)
-
-			await ctx.send("Someone died")
-
+			await self.fighting(ctx, member, p1, p2)
 		finally: 
 			# resets all the variables
 			self.embed = discord.Embed(color=0x24ecf7, title="Pit Boss' Wrestling | FIGHT!")
@@ -97,13 +52,58 @@ class Fight(commands.Cog):
 		if p1Lvl < p2Lvl:return False
 		else:return True
 
-	def checkGameOver(self, p1, p2): # checks if either player is dead
-		if p1[0] <= 0 or p2[0] <= 0:
-			print(p1[0])
-			print(p2[0])
-			return True
+	async def fighting(self, ctx, member, p1, p2):
+		turnNum = 0
+		while True:
+			if turnNum % 2 == 0: # player 1's turn
+				self.embed.color = discord.Color(0x007efd)
+				player = [p1, ctx.author.name]
+				opponent = [p2, member.name]
+
+			else:  # player 2's turn
+				self.embed.color = discord.Color(0xfd0006)
+				player = [p2, ctx.author.name]
+				opponent = [p1, member.name]
+
+			act = randrange(100) # generate what they'll do on their turn
+			if act <= 70: # 70% chance to attack; dmg amount based on generated #
+				opponent[0][0] -= act + 20
+				self.embed.add_field(name="DAMAGE", value=f"{player[1]} damaged {opponent[1]} for {act + 20} damage!\nHe has {opponent[0][0]} health left!")
+				await asyncio.sleep(2)
+			elif act > 70: # 30% chance to heal; heal amount based on generated #
+				if player[0][3] > 0:
+					player[0][3] -= 1
+					player[0][0] += act
+					self.embed.add_field(name="HEALED", value=f"{player[1]} healed for {act} health!\nHealth Remaining: {player[0][0]}\nPotions remaining: {player[0][3]}")
+				else:
+					self.embed.add_field(name="HEALED", value=f"{player[1]}tried to use a health potion, but realized he has none.")
+				await asyncio.sleep(2)
+
+			print(act)
+			
+			# sends embed and resets it for next turn
+			await ctx.send(embed=self.embed)
+			self.embed.clear_fields()
+
+			turnNum += 1 # changes whose turn it is
+
+			dead = self.checkGameOver(player, opponent)
+			if dead:
+				#print("broke")
+				break # will break once player is dead
+			await asyncio.sleep(2)
+
+		await ctx.send(f"{dead} has been killed!")
+
+
+	def checkGameOver(self, player, opponent): # checks if either player is dead
+		if player[0][0] <= 0:
+			return player[1]
+
+		elif opponent[0][0] <= 0:
+			return opponent[1]
 		else:
-			return False
+			return None
 
 
 
