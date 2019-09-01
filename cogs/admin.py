37c2a3a84e82e9bccdba6d1bbb3403cf13533a92
MrBizWiz3494 <wilsonis123@icloud.com>
2019-09-01 13:26:57 -0500

    Removed checkFailure

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions, CheckFailure
+from discord.ext.commands import has_permissions
 
 class Admin(commands.Cog):
 	def __init__(self, bot):
